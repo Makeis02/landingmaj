5bb15fb1feeaac46ca81f1bf05e20747b394b5e0
Makeis02 <smn02300@gmail.com>
2025-06-08 21:30:05 +0200

    Ton message clair icidsqdsdqdffdqsdgdqsdqdasdadqsdqdqsdq

--- supabase/functions/subscribe-to-newsletter/index.ts
+++ supabase/functions/subscribe-to-newsletter/index.ts
@@ -2,8 +2,8 @@ import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-  'Access-Control-Allow-Methods': 'POST, OPTIONS'
+  'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, access-control-allow-headers, access-control-allow-methods, access-control-allow-origin',
 };
 
 // Remplace par ta clé API Omisend dans les variables d'environnement Supabase
