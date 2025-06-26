65002ef99ad81c353399b4a2b7a94faf1bd93624
Makeis02 <smn02300@gmail.com>
2025-06-26 07:55:44 +0200

    B035

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import AuthProvider from './components/AuthProvider'
 import './index.css'
+import clarity from '@microsoft/clarity'
+
+clarity.init("otu0u5aqbj")
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
