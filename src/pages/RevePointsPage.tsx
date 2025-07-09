8d0a2db340a1995c6d4274385fb3b61cbcc2f78a
Makeis02 <smn02300@gmail.com>
2025-07-09 11:02:06 +0200

    revepoint3

--- src/pages/RevePointsPage.tsx
+++ src/pages/RevePointsPage.tsx
@@ -6,20 +6,28 @@ import RevePointsHero from '@/components/revepoints/RevePointsHero';
 import HowItWorks from '@/components/revepoints/HowItWorks';
 import YourAdvantages from '@/components/revepoints/YourAdvantages';
 import SimpleRules from '@/components/revepoints/SimpleRules';
-import BoostYourPoints from '@/components/revepoints/BoostYourPoints';
 import FinalCTA from '@/components/revepoints/FinalCTA';
 
 const RevePointsPage = () => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-cyan-50 to-blue-50">
       <Header />
-      <main className="pt-32">
-        <RevePointsHero />
-        <HowItWorks />
-        <YourAdvantages />
-        <SimpleRules />
-        <BoostYourPoints />
-        <FinalCTA />
+      <main className="pt-8">
+        <div className="animate-fade-in-up duration-700">
+          <RevePointsHero />
+        </div>
+        <div className="animate-fade-in-up duration-700 delay-100">
+          <HowItWorks />
+        </div>
+        <div className="animate-fade-in-up duration-700 delay-200">
+          <YourAdvantages />
+        </div>
+        <div className="animate-fade-in-up duration-700 delay-300">
+          <SimpleRules />
+        </div>
+        <div className="animate-fade-in-up duration-700 delay-400">
+          <FinalCTA />
+        </div>
       </main>
       <Footer />
     </div>
